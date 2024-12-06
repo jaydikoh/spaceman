09573ed693124f2b5a5a64f1ab4380e57f3669a1
jaydikoh <ekwensijason@gmail.com>
2024-12-06 06:42:39 -0500

    spaceman complete

--- js/main.js
+++ js/main.js
@@ -24,13 +24,13 @@ let pressedKeys = [];
 
 /*----- cached elements  -----*/
 
-const letterContainer = document.querySelector(".word"); //selecting divs were guessed word will appear
-const livesEl = document.getElementById("lives");        //selcting h3 that show how many wrong guesses
-const hintEl = document.querySelector("#hint");          //selecting hint element
-const keyboardButtons = document.querySelectorAll(".keyboard-buttons"); //selecting all the buttons on the heyboard
-const gameOverEl = document.querySelector(".gameover");  //selecting the gameover element
-const playAgainButton = document.querySelector(".try-again"); //selecting the play-again button
-const gameImageEl = document.getElementById("game-image"); // Select the game image container
+const letterContainer = document.querySelector(".word");                   //selecting divs were guessed word will appear
+const livesEl = document.getElementById("lives");                          //selcting h3 that show how many wrong guesses
+const hintEl = document.querySelector("#hint");                            //selecting hint element
+const keyboardButtons = document.querySelectorAll(".keyboard-buttons");    //selecting all the buttons on the heyboard
+const correctWordEl = document.querySelector(".correct-word");                    //selecting the gameover element
+const playAgainButton = document.querySelector(".try-again");              //selecting the play-again button
+const gameImageEl = document.getElementById("game-image");                 //Select the game image container
 const manImage = document.getElementById('spaceman');
 const body = document.querySelector('body');
 const winOrLose = document.querySelector('.checkwinner')
@@ -49,45 +49,43 @@ playAgainButton.addEventListener('click', init);
 function init() {
   selectedWord = words[Math.floor(Math.random() * words.length)];       //math.random pick a index number at random, math.floor makes it a whole number
   guessedLetters = [];
-  pressedKeys = [];            //put the gussed letters in an Array
-  remainingGuesses = 7;              // Hide game-over message
-  gameOverEl.style.visibility = 'hidden';
+  pressedKeys = [];                                               //put the gussed letters in an Array
+  remainingGuesses = 7;                                           // Hide game-over message
+  correctWordEl.style.visibility = 'hidden';
   keyboardButtons.forEach((button) => {
     button.disabled = false;                                      // Re-enable all buttons
     button.style.backgroundColor = "";                            //reset button style
   });
-  hintEl.textContent = `HINT: ${selectedWord.hint}`;      //selects hint for chosen word
-  livesEl.innerHTML = `Attempts left: <b>${remainingGuesses}`
-  letterContainer.innerHTML = "";                           //Clear existing `letter-word` divs
+  hintEl.textContent = `HINT: ${selectedWord.hint}`;              //selects hint for chosen word
+  livesEl.innerHTML = `Attempts left: <b>${remainingGuesses}`;
+  letterContainer.innerHTML = "";                                 //Clear existing `letter-word` divs
   manImage.src = "img/spaceman-0.png";
   manImage.style.visibility = "visible";
   winOrLose.innerText = "";
-  render();                   //renedr blank spaces
+  render();                                                       //render 
 }
 
 function render() {
   wordsDisplay();
   body.addEventListener('keydown', handleClick);
-
 }
-
 
 // this function displays blank spaces representing the div elements
 function wordsDisplay() {
-  const wordArray = selectedWord.word.split("");         //spliting the array into individual letters
-  wordArray.forEach(() => {                              //iterating through the split word
-    const letterDiv = document.createElement('div');   //each letter creates a new div to hold the letter, this is in order to adapt to the number of letters in the word
-    letterDiv.className = "letter-word";   //Assigns the letter-word class to the newly created div
-    letterDiv.textContent = ""; // Initially display an empty div until player chooses letter
-    letterContainer.appendChild(letterDiv);            //Appends the newly created div to the letterContainer
+  const wordArray = selectedWord.word.split("");                 //spliting the array into individual letters
+  wordArray.forEach(() => {                                      //iterating through the split word
+    const letterDiv = document.createElement('div');             //each letter creates a new div to hold the letter, this is in order to adapt to the number of letters in the word
+    letterDiv.className = "letter-word";                         //Assigns the letter-word class to the newly created div
+    letterDiv.textContent = "";                                  // Initially display an empty div until player chooses letter
+    letterContainer.appendChild(letterDiv);                      //Appends the newly created div to the letterContainer
   });
 }
 
 function updateWordDisplay() {
   const letterDivs = document.querySelectorAll(".letter-word");  //Selects all div elements with the class letter-word 
-  selectedWord.word.split("").forEach((letter, index) => {   //Splits the selectedWord.word string into an array of letters, Iterates over each letter using forEach. index: The position of the letter in the word.
-    if (guessedLetters.includes(letter)) {                         //Checks whether the current letter is in the guessedLetters array
-      letterDivs[index].textContent = letter; // Reveal the guessed letter
+  selectedWord.word.split("").forEach((letter, index) => {       //Splits the selectedWord.word string into an array of letters, Iterates over each letter using forEach. index: The position of the letter in the word.
+    if (guessedLetters.includes(letter)) {                       //Checks whether the current letter is in the guessedLetters array
+      letterDivs[index].textContent = letter;                    // Reveal the guessed letter
     }
   });
 }
@@ -96,24 +94,24 @@ function getWinner() {
   const letterDivs = document.querySelectorAll(".letter-word");     //Selects all div elements with the class letter-word
   const wordArray = selectedWord.word.split("");
   if (remainingGuesses <= 0) {
-    gameOverEl.style.display = "block";
-    gameOverEl.textContent = `The word was "${selectedWord.word}"`;
-    gameOverEl.style.visibility = 'visible';
+    correctWordEl.style.display = "block";
+    correctWordEl.textContent = `The word was "${selectedWord.word}"`;
+    correctWordEl.style.visibility = 'visible';
     winOrLose.innerText = "YOU LOSE";
-    disableKeyboard();
-    loseSound.play();                                           //disables keyboard if game losses
+    disableKeyboard();                                              //disables keyboard if game losses
+    loseSound.play();
   }
   else if (wordArray.every((letter, index) => letterDivs[index].textContent === letter)) {   //Uses every() to check if all letters in the word (wordArray) match the corresponding letters displayed in the letterDivs.
-    gameOverEl.style.display = "block";
-    gameOverEl.style.visibility = 'visible';
+    correctWordEl.style.display = "block";
+    correctWordEl.style.visibility = 'visible';
     winOrLose.innerText = "YOU WIN";
     manImage.style.visibility = "hidden";
     disableKeyboard();
     winSound.play()
     confetti({
-      particleCount: 400, // Number of confetti pieces
-      spread: 100,        // Angle of spread
-      origin: { x: 0.5, y: 0.3 }, // Start position
+      particleCount: 400,                                          // Number of confetti pieces
+      spread: 100,                                                 // Angle of spread
+      origin: { x: 0.5, y: 0.3 },                                  // Start position
     });
   }
 }
@@ -126,11 +124,11 @@ function disableKeyboard() {
 }
 
 function handleClick(event) {
-  let guessedLetter;                                         //Retrieves the text content of the button clicked by the player.
-  if (event.type === 'click') {                               //Retrieves the text content of the button clicked by the player.
+  let guessedLetter;                                            //Retrieves the text content of the button clicked by the player.
+  if (event.type === 'click') {                                 //Retrieves the text content of the button clicked by the player.
     guessedLetter = event.target.textContent.toLowerCase();
     event.target.disabled = true;
-    keystroke = event.target                                  //Disables the button so the player cannot select the same letter again.
+    keystroke = event.target                                    //Disables the button so the player cannot select the same letter again.
   } else {
     guessedLetter = event.key.toLowerCase();
     if (!acceptedKeys.includes(guessedLetter)) return;
@@ -139,15 +137,15 @@ function handleClick(event) {
     keyboardButtons.forEach((button) => {
       if (button.textContent.toLowerCase() === guessedLetter) {
         keystroke = button;
-        keystroke.disabled = true;                       //Disables the button so the player cannot select the same letter again.  
+        keystroke.disabled = true;                             //Disables the button so the player cannot select the same letter again.  
       }
     })
   }
 
-  pressedKeys.push(guessedLetter);                       //add the key to the pressedKeys array
+  pressedKeys.push(guessedLetter);                             //add the key to the pressedKeys array
 
   if (selectedWord.word.includes(guessedLetter)) {
-    guessedLetters.push(guessedLetter);              // Add correct guess to guessedLetters array
+    guessedLetters.push(guessedLetter);                        // Add correct guess to guessedLetters array
     if (event.type === "click") event.target.style.backgroundColor = "green"; // Indicate correct guess
     keystroke.style.backgroundColor = "green";
     correctSound.currentTime = 0;
